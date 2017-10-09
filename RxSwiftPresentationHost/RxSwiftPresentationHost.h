fc4ad2e0d770a8978b836627fe5d75d6b52c500b
HeMet <hemet.mail@gmail.com>
2017-10-09 20:30:49 +0400

    Initial Commit

---
+++ RxSwiftPresentationHost/RxSwiftPresentationHost.h
@@ -0,0 +1,19 @@
+//
+//  RxSwiftPresentationHost.h
+//  RxSwiftPresentationHost
+//
+//  Created by Evgeniy Gubin on 09.10.17.
+//  Copyright © 2017 Eugene Gubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSwiftPresentationHost.
+FOUNDATION_EXPORT double RxSwiftPresentationHostVersionNumber;
+
+//! Project version string for RxSwiftPresentationHost.
+FOUNDATION_EXPORT const unsigned char RxSwiftPresentationHostVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftPresentationHost/PublicHeader.h>
+
+
